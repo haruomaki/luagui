af7ada8ac9028863e0775d538331d379fd65e0d8
Haruo <haruo-mtok@outlook.com>
2023-06-22 20:27:26 +0900

    ソート

--- GUI/include/master.hpp
+++ GUI/include/master.hpp
@@ -1,24 +1,16 @@
 #pragma once
 
+#include <Camera.hpp>
 #include <Draw.hpp>
+#include <DynamicArray.hpp>
 #include <GUI.hpp>
 #include <KeyCallback.hpp>
+#include <Polygon.hpp>
 #include <Shader.hpp>
 #include <SizeCallback.hpp>
+#include <Text.hpp>
 #include <Timer.hpp>
 #include <Update.hpp>
 #include <Window.hpp>
 #include <World.hpp>
 #include <WorldObject.hpp>
-
-template <typename T>
-struct Rect {
-    T left_, right_, bottom_, top_;
-};
-
-// Windowを利用するヘッダ。Windowの宣言のあとにインクルード
-#include <Camera.hpp>
-
-#include <DynamicArray.hpp>
-#include <Polygon.hpp>
-#include <Text.hpp>
