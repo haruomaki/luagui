3b8475ee86c4478dde15446c2695a14735895395
Haruo <haruo-mtok@outlook.com>
2023-06-25 19:23:53 +0900

    usingと継承実験

--- src/bin/inherit.cpp
+++ src/bin/inherit.cpp
@@ -1,37 +1,17 @@
-#include <GUI.hpp>
+#include <base.hpp>
 
-template <typename SelfType>
-struct Base {
-    SelfType *getThis() {
-        return static_cast<SelfType *>(this);
-    }
+template <auto x>
+class Base {
+    using PrivateType = float;
+
+  protected:
+    using MyType = decltype(x);
 };
 
-struct Derived1 : Base<Derived1> {
-    int v = 0;
-
-    void show() {
-        debug(v);
-    }
-};
-
-struct Derived2 : Base<Derived1> {
-    int v = 0;
-
-    void show() const {
-        debug(v * 100);
-    }
+template <auto x>
+class Derived : Base<334> {
+    MyType y;
 };
 
 int main() {
-    Derived1 d1;
-    Derived2 d2;
-
-    auto *p = d1.getThis();
-    p->v = 100;
-    p->show();
-
-    auto *p2 = d2.getThis();
-    p2->v = 364;
-    p2->show();
 }
