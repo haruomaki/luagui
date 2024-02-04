9b16cc55885431f55edd9658ecb13a01c07e1da3
Haruo <haruo-mtok@outlook.com>
2024-02-05 03:02:03 +0900

    README追記＆コルーチンテスト＆微改善

--- GUI/include/Window.hpp
+++ GUI/include/Window.hpp
@@ -40,8 +40,8 @@ class Window {
     [[nodiscard]] bool getKey(int key) const;
 
     template <typename T, typename... Args>
-        requires std::is_constructible_v<T, Args...> && // ArgsはTのコンストラクタの引数
-                 std::is_base_of_v<SizeCallback, T>     // TはSizeCallbackの派生クラス
+        requires std::is_constructible_v<T, Args...> &&     // ArgsはTのコンストラクタの引数
+                 std::is_convertible_v<T *, SizeCallback *> // TはSizeCallbackの派生クラス
     T &makeChild(Args &&...args) {
         // argsを引数として使って、ヒープ上にT型のオブジェクトを作成
         auto ptr = std::make_unique<T>(std::forward<Args>(args)...); // NOTE: &&やforwardは必要かよく分からない
