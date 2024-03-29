04ded72fce5a33c07796c11df5463ccf578c2020
Haruo <haruo-mtok@outlook.com>
2024-03-29 23:08:03 +0900

    [WIP] MeshDrawManager作成

--- GUI/include/Mesh.hpp
+++ GUI/include/Mesh.hpp
@@ -9,9 +9,10 @@
 // VRAMとの同期を毎フレーム自動で行わないメッシュ
 class StaticMesh : virtual public Resource {
     friend class MeshObject;
+    friend struct MeshDrawManager;
 
   protected:
-    const VertexArrayObject vao_;
+    bool vao_should_regen_ = true;
     VertexBufferObject vbo_;
     ElementBufferObject ebo_;
     const Material &material_;
@@ -22,31 +23,17 @@ class StaticMesh : virtual public Resource {
     size_t indices_capacity_ = 0;
     static constexpr RGBA default_color{0.8f, 0.8f, 0.8f, 1};
 
-    void regen_vao() {
-        // VAOに頂点の座標と色を関連付ける
-        vao_.bind([&] {
-            vbo_.bind([&] {
-                const auto &shader = this->material_.shader;
-                shader.set_attribute("position", 3, GL_FLOAT, GL_FALSE, sizeof(InterleavedVertexInfo), nullptr);                                  // 位置
-                shader.set_attribute("color", 4, GL_FLOAT, GL_FALSE, sizeof(InterleavedVertexInfo), reinterpret_cast<void *>(sizeof(float) * 3)); // 色 offset=12 NOLINT(performance-no-int-to-ptr)
-                shader.set_attribute("uv", 2, GL_FLOAT, GL_FALSE, sizeof(InterleavedVertexInfo), reinterpret_cast<void *>(28));
-            });
-            ebo_.keep_bind();
-            getErrors();
-        });
-    }
-
     void regenerate_vbo() {
         print("VBO生成");
         // 空のVBOを生成
         vbo_ = VertexBufferObject::gen(sizeof(InterleavedVertexInfo) * vertices.capacity(), nullptr, usage_);
-        regen_vao();
+        vao_should_regen_ = true;
     }
 
     void regenerate_ibo() {
         print("IBO生成");
         this->ebo_ = ElementBufferObject::gen(sizeof(int) * indices.size(), indices.data(), usage_);
-        regen_vao();
+        vao_should_regen_ = true;
     }
 
   public:
@@ -54,8 +41,7 @@ class StaticMesh : virtual public Resource {
     std::vector<int> indices;
 
     StaticMesh(const Material *material = nullptr, const vector<glm::vec3> &coords = {}, const vector<RGBA> &colors = {}, const vector<glm::vec2> &uvs = {}, GLenum usage = GL_STATIC_DRAW)
-        : vao_(VertexArrayObject::gen())
-        , material_(material == nullptr ? *this->get_window().default_material : *material)
+        : material_(material == nullptr ? *this->get_window().default_material : *material)
         , usage_(usage)
         , n_(coords.size())
         , capacity_(coords.capacity()) {
@@ -167,11 +153,42 @@ class MeshObject : public Draw {
         , use_index(use_index) {}
 
     void draw(const Camera &camera) const override {
+        // FIXME: register_to_drawを呼ぶ
+    }
+};
+
+struct MeshDrawManager {
+    std::map<std::tuple<const StaticMesh *, const Material *, const ProgramObject *>, std::pair<VertexArrayObject, vector<glm::mat4>>> vao_modelmatrices;
+
+    static VertexArrayObject regenerate_vao(StaticMesh &mesh, const ProgramObject &shader) {
+        auto vao = VertexArrayObject::gen();
+
+        // VAOに頂点の座標と色を関連付ける
+        vao.bind([&] {
+            mesh.vbo_.bind([&] {
+                shader.set_attribute("position", 3, GL_FLOAT, GL_FALSE, sizeof(InterleavedVertexInfo), nullptr);                                  // 位置
+                shader.set_attribute("color", 4, GL_FLOAT, GL_FALSE, sizeof(InterleavedVertexInfo), reinterpret_cast<void *>(sizeof(float) * 3)); // 色 offset=12 NOLINT(performance-no-int-to-ptr)
+                shader.set_attribute("uv", 2, GL_FLOAT, GL_FALSE, sizeof(InterleavedVertexInfo), reinterpret_cast<void *>(28));
+            });
+            mesh.ebo_.keep_bind();
+            getErrors();
+        });
+
+        mesh.vao_should_regen_ = false; // VAOの再生成が完了
+        return vao;
+    }
+
+    static inline void draw_one(const glm::mat4 &model_matrix, const StaticMesh &mesh, const VertexArrayObject vao, const Camera &camera) {
+        const Material &material = mesh.material_;
+
         // シェーダを有効化
-        const auto &shader = this->mesh.material_.shader;
+        const auto &shader = material.shader;
         shader.use();
 
         // 点の大きさ・線の太さを設定
+        auto draw_mode = GL_LINE_STRIP; // FIXME: ダミー
+        auto point_size = 10.f;         // FIXME: ダミー
+        auto line_width = 10.f;         // FIXME: ダミー
         switch (draw_mode) {
         case GL_POINTS:
             glPointSize(point_size);
@@ -183,8 +200,7 @@ class MeshObject : public Draw {
             break;
         }
 
-        // ワールド座標変換
-        const glm::mat4 &model_matrix = this->get_absolute_transform();
+        // ワールド座標変換 = model_matrix
 
         // ビュー座標変換
         const glm::mat4 &view_matrix = camera.get_view_matrix();
@@ -198,18 +214,63 @@ class MeshObject : public Draw {
         shader.set_uniform("projectionMatrix", projection_matrix);
 
         // モデルの描画
-        GLuint tex_id = mesh.material_.texture.value_or(0);
+        GLuint tex_id = material.texture.value_or(0);
         shader.set_uniform("is_tex", (tex_id == 0 ? GL_FALSE : GL_TRUE));
-        this->mesh.vao_.bind([&] {
+        vao.bind([&] {
             glBindTexture(GL_TEXTURE_2D, tex_id); // テクスチャを指定
+            auto use_index = false;               // FIXME:ダミー
             if (use_index) {
-                size_t indices_length = this->mesh.indices_n_;
+                size_t indices_length = mesh.indices_n_;
                 glDrawElements(draw_mode, GLsizei(sizeof(int) * indices_length), GL_UNSIGNED_INT, nullptr);
             } else {
-                size_t vertices_length = this->mesh.n_;
+                size_t vertices_length = mesh.n_;
                 glDrawArrays(draw_mode, 0, GLsizei(vertices_length));
             }
             glBindTexture(GL_TEXTURE_2D, 0); // テクスチャのバインドを解除
         });
+    }
+
+    void register_to_draw(const MeshObject &obj, const Material &material) {
+        auto &mesh = obj.mesh;
+        const auto &shader = material.shader;
+        const StaticMesh *mp = &mesh;
+        const Material *tp = &material;
+        const ProgramObject *sp = &shader;
+        const auto key = std::make_tuple(mp, tp, sp); // メッシュ・マテリア・シェーダの三項組をキーとする
+
+        if (!vao_modelmatrices.contains(key)) {
+            // キャッシュに未登録ならばVAOを新規作成する
+            auto vao = regenerate_vao(mesh, shader);
+            vao_modelmatrices[key] = std::make_pair(vao, std::vector<glm::mat4>(0));
+        } else if (mesh.vao_should_regen_) {
+            // もしくはVBOの更新などでVAOの再生成が必要な場合もある
+            vao_modelmatrices[key].first = regenerate_vao(mesh, shader);
+        }
+
+        // モデル行列をキューに追加
+        const glm::mat4 &model_matrix = obj.get_absolute_transform();
+        vao_modelmatrices[key].second.push_back(model_matrix);
+    }
+
+    void draw_all_registered_objects(const Camera &camera) {
+        for (auto it = vao_modelmatrices.begin(); it != vao_modelmatrices.end();) {
+            auto &[key, value] = *it;
+            const StaticMesh &mesh = *std::get<0>(key);
+            const VertexArrayObject vao = value.first;
+            std::vector<glm::mat4> &model_matrices = value.second;
+
+            // もはや使われなくなったVAOは削除し、次のキーへ
+            if (model_matrices.size() == 0) {
+                it = vao_modelmatrices.erase(it);
+            } else {
+                // 各モデル行列についてドローコールを発行
+                for (const auto &model_matrix : model_matrices) {
+                    draw_one(model_matrix, mesh, vao, camera);
+                }
+                // 描画を終えたモデル行列のキューは空に
+                model_matrices.clear();
+                it++;
+            }
+        }
     }
 };
