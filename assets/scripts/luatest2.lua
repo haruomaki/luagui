38c774ef3a951641c203572b8702f7811fa7b65b
Haruo <haruo-mtok@outlook.com>
2024-09-12 12:02:55 +0900

    C++からコルーチンを実行する方法

--- assets/scripts/luatest2.lua
+++ assets/scripts/luatest2.lua
@@ -1,1 +1,20 @@
-print("こんにちわ")
+-- local function title()
+--     print("タイトルです")
+--     coroutine.yield()
+-- end
+
+-- create_window(100, 100, "たいとるお", function()
+--     poyo()
+--     title()
+--     while true do
+--         coroutine.yield()
+--     end
+-- end)
+function coro_func()
+    for i = 1, 5 do
+        print("Step: " .. i)
+        coroutine.yield() -- コルーチンを一時停止
+    end
+end
+
+create_window(800, 600, "Test Window", coro_func)
--- src/bin/luatest.cpp
+++ src/bin/luatest.cpp
@@ -3,9 +3,46 @@
 
 int main() {
     LuaGUI luagui;
-    luagui.mainloop("assets/scripts/luatest2.lua");
+    luagui.run("assets/scripts/luatest2.lua");
 
     testfunc();
 
     run_lua("assets/scripts/luatest.lua");
 }
+
+// #include <sol/sol.hpp>
+
+// #include <iostream>
+
+// int main() {
+//     std::cout << "=== coroutine ===" << std::endl;
+
+//     sol::state lua;
+
+//     lua.open_libraries(sol::lib::base, sol::lib::coroutine);
+
+//     lua.set_function("start_task",
+//                      [&lua](
+//                          sol::function f) {
+//                          // You must ALWAYS get the current state
+//                          sol::thread runner_thread = sol::thread::create(lua);
+//                          sol::state_view runner_thread_state = runner_thread.state();
+//                          sol::coroutine f_on_runner_thread(runner_thread_state, f);
+
+//                          f_on_runner_thread();
+//                          f_on_runner_thread();
+//                      });
+
+//     lua.script(
+//         R"(
+// function main()
+// 	print("りんご")
+// 	coroutine.yield(20)
+// 	print("みかん")
+// end
+
+// 	start_task(main)
+// )");
+
+//     return 0;
+// }
