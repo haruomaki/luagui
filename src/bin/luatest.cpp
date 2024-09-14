af548cdfad44f45a37caa986aabb7cf82ff7b4a7
Haruo <haruo-mtok@outlook.com>
2024-09-14 18:39:36 +0900

    stackoverflowの回答作成用
    https://stackoverflow.com/questions/78974545/how-to-run-a-lua-coroutine-in-c-with-sol3/78984781#78984781

--- src/bin/luatest.cpp
+++ src/bin/luatest.cpp
@@ -1,48 +1,40 @@
-// #include <lua_load.hpp>
-#include <luagui.hpp>
-
-int main() {
-    LuaGUI luagui;
-    luagui.run("assets/scripts/luatest2.lua");
-
-    // testfunc();
-
-    // run_lua("assets/scripts/luatest.lua");
-}
-
-// #include <sol/sol.hpp>
-
-// #include <iostream>
+// // #include <lua_load.hpp>
+// #include <luagui.hpp>
 
 // int main() {
-//     std::cout << "=== coroutine ===" << std::endl;
+//     LuaGUI luagui;
+//     luagui.run("assets/scripts/luatest2.lua");
 
-//     sol::state lua;
+//     // testfunc();
 
-//     lua.open_libraries(sol::lib::base, sol::lib::coroutine);
-
-//     lua.set_function("start_task",
-//                      [&lua](
-//                          sol::function f) {
-//                          // You must ALWAYS get the current state
-//                          sol::thread runner_thread = sol::thread::create(lua);
-//                          sol::state_view runner_thread_state = runner_thread.state();
-//                          sol::coroutine f_on_runner_thread(runner_thread_state, f);
-
-//                          f_on_runner_thread();
-//                          f_on_runner_thread();
-//                      });
-
-//     lua.script(
-//         R"(
-// function main()
-// 	print("りんご")
-// 	coroutine.yield(20)
-// 	print("みかん")
-// end
-
-// 	start_task(main)
-// )");
-
-//     return 0;
+//     // run_lua("assets/scripts/luatest.lua");
 // }
+
+#include <sol/sol.hpp>
+
+int main() {
+    sol::state lua;
+    lua.open_libraries(sol::lib::base, sol::lib::coroutine);
+
+    lua.set_function("run_coroutine", [&lua](sol::function f) {
+        sol::thread runner_thread = sol::thread::create(lua);
+        sol::state_view runner_thread_state = runner_thread.state();
+        sol::coroutine co(runner_thread_state, f);
+
+        co();
+        co();
+        co();
+    });
+
+    lua.script(R"(
+        function story()
+            print("1")
+            coroutine.yield()
+            print("2")
+            coroutine.yield()
+            print("3")
+        end
+
+        run_coroutine(story)
+    )");
+}
