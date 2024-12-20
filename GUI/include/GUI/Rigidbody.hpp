1e2ff97f0d15afcbc35e9e49d6a678738d650be6
Haruo <haruo-mtok@outlook.com>
2024-12-21 02:50:06 +0900

    ColliderComponentが独立して存在できるようにすることで、削除順に起因する実行時エラーを解消（未完？）

--- GUI/include/GUI/Rigidbody.hpp
+++ GUI/include/GUI/Rigidbody.hpp
@@ -5,6 +5,9 @@
 #include <optional>
 #include <variant>
 
+/// 一つの剛体を表すコンポーネント。
+/// オブジェクトに対してRigidbodyComponentは一つだけ付与できる。
+/// ColliderComponentだけ事前に追加しておくことも可能。その場合はRigidbodyComponentが追加されると同時にShapeも適用される。
 class RigidbodyComponent : public Component {
   public:
     b2::Body b2body;
@@ -16,7 +19,21 @@ class RigidbodyComponent : public Component {
 // Box2DのShapeたちをまとめるタグ付きユニオン
 using ShapeVariant = std::variant<b2Circle, b2Capsule, b2Segment, b2Polygon>;
 
+/// 剛体に付随する当たり判定を表すコンポーネント。
+/// 一つの剛体に対して複数のコライダーを追加できる。
+/// RigidbodyComponentが既に存在するときは即座にShapeが追加されるが、そうでないときはRigidbodyComponentが追加され次第Shapeも速やかに反映される。
 class ColliderComponent : public Component {
+    bool active_ = false; // Shapeが実際にBodyに付けられているか
+
+    friend class RigidbodyComponent;
+    ShapeVariant shape_;
+    b2::Shape::Params shape_params_;
+
+    // RigidBodyがあるときだけ呼び出す。その直下にShapeを追加する。
+    void append_shape(RigidbodyComponent *rbc);
+    // RigidBodyがあるときだけ呼び出す。その直下からShapeを削除する。
+    void drop_shape(RigidbodyComponent *rbc);
+
   public:
     // NOLINTNEXTLINE(readability-identifier-naming)
     b2::ShapeRef shape_ref_; // publicだが直接触るのは非推奨
--- GUI/src/Rigidbody.cpp
+++ GUI/src/Rigidbody.cpp
@@ -8,21 +8,29 @@
 RigidbodyComponent::RigidbodyComponent(b2::Body::Params body_params) {
     trace("RigidbodyComponentのコンストラクタです。", this);
 
+    if (owner().get_components<RigidbodyComponent>().size() != 0) throw std::runtime_error("１つの物体に２つ以上のRigidbodyを付けることは不正です。");
+
     // Worldのrigidbodyリストに追加
     world().rigidbody_components.request_set(this);
 
     auto &b2world = world().b2world;
     b2::Body tmp_body = b2world.CreateBody(b2::OwningHandle, body_params);
     b2body = std::move(tmp_body);
+
+    // 既にColliderが付いていたら、Shapeを遅延的に作成。
+    for (auto *cc : owner().get_components<ColliderComponent>()) {
+        cc->append_shape(this);
+    }
 }
 
 RigidbodyComponent::~RigidbodyComponent() {
     trace("RigidbodyComponentのデストラクタです。", this);
+
+    // Shapeを外す
     auto ccs = owner().get_components<ColliderComponent>();
-    // debug(ccs);
     for (auto *cc : ccs) {
-        print("けすよ", cc->shape_ref_.Handle().index1, ", ", cc);
-        cc->erase();
+        info("ShapeはあるけどRigidbodyを消すよ。handle: ", cc->shape_ref_.Handle().index1, ", ", cc);
+        cc->drop_shape(this);
     }
 
     // ChainColliderComponentも消す
@@ -52,20 +60,41 @@ static RigidbodyComponent &get_rigidbody(Comp *self) {
     return *rbcs[0];
 }
 
-ColliderComponent::ColliderComponent(ShapeVariant shape, b2::Shape::Params shape_params) {
+void ColliderComponent::append_shape(RigidbodyComponent *rbc) {
     // どの型が代入されていたとしても、共通のインタフェースを呼び出す
     std::visit(
         [&](auto &s) {
-            auto &rbc = get_rigidbody(this);
-            shape_params.userData = static_cast<void *>(this);
-            shape_ref_ = rbc.b2body.CreateShape(b2::DestroyWithParent, shape_params, s);
+            active_ = true;
+            shape_params_.userData = static_cast<void *>(this);
+            shape_ref_ = rbc->b2body.CreateShape(b2::DestroyWithParent, shape_params_, s);
         },
-        shape);
+        shape_);
+}
+
+void ColliderComponent::drop_shape(RigidbodyComponent * /*rbc*/) {
+    active_ = false;
+    this->shape_ref_.Destroy();
+}
+
+ColliderComponent::ColliderComponent(ShapeVariant shape, b2::Shape::Params shape_params)
+    : shape_(shape)
+    , shape_params_(shape_params) {
+    auto rbcs = owner().get_components<RigidbodyComponent>();
+    if (rbcs.size() == 0) {
+        info("RigidbodyComponentが無いため、Shapeを付けずに保留。");
+    } else {
+        this->append_shape(rbcs[0]);
+    }
 }
 
 ColliderComponent::~ColliderComponent() {
     trace("start ColliderComponent dtor");
-    this->shape_ref_.Destroy();
+    auto rbcs = owner().get_components<RigidbodyComponent>();
+    if (rbcs.size() == 0) {
+        info("RigidbodyComponentが無いため、そのまま終了。");
+    } else {
+        this->drop_shape(rbcs[0]);
+    }
     trace("finish ColliderComponent dtor");
 }
 
