a88a247fcc58642a246260c50f65e290769d9bdd
Haruo <haruo-mtok@outlook.com>
2024-12-20 16:25:36 +0900

    UpdateComponentにDrawの機能を統合

--- GUI/include/GUI/Update.hpp
+++ GUI/include/GUI/Update.hpp
@@ -4,9 +4,10 @@
 
 class UpdateComponent : public Component {
     std::function<void()> func_;
+    const std::string category_;
 
   public:
-    UpdateComponent(std::function<void(UpdateComponent &)> &&f);
+    UpdateComponent(std::function<void(UpdateComponent &)> &&f, std::string category = "Update");
     ~UpdateComponent() override;
     UpdateComponent(const UpdateComponent &) = delete;
     UpdateComponent &operator=(const UpdateComponent &) const = delete;
--- GUI/src/Update.cpp
+++ GUI/src/Update.cpp
@@ -1,10 +1,20 @@
 #include "Update.hpp"
 #include "World.hpp"
 
-UpdateComponent::UpdateComponent(std::function<void(UpdateComponent &)> &&f) {
-    this->func_ = [f, this] { trace("lambda from UpdateComponent (id: ", this->id, ")"); f(*this); trace("lambda ended"); };
-    world().updates.request_set(&this->func_);
+UpdateComponent::UpdateComponent(std::function<void(UpdateComponent &)> &&f, std::string category)
+    : category_(std::move(category)) {
+    this->func_ = [f, this] { trace("lambda from UpdateComponent:", this->category_, " (id: ", this->id, ")"); f(*this); trace("lambda ended"); };
+    if (category_ == "Update") {
+        world().updates.request_set(&this->func_);
+    } else if (category_ == "Draw") {
+        world().draws.request_set(&this->func_);
+    }
 }
+
 UpdateComponent::~UpdateComponent() {
-    world().updates.request_erase(&this->func_);
+    if (category_ == "Update") {
+        world().updates.request_erase(&this->func_);
+    } else if (category_ == "Draw") {
+        world().draws.request_erase(&this->func_);
+    }
 }
