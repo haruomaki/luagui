5598fc4d2cd22dde66b03657cf28d7e8903dd772
Haruo <haruo-mtok@outlook.com>
2024-12-23 03:28:57 +0900

    再生成功！

--- src/bin/lunchbox_test1.cpp
+++ src/bin/lunchbox_test1.cpp
@@ -8,7 +8,7 @@
 #include <lunchbox.hpp>
 
 // メモリ上のデータを操作するための構造体
-struct MemoryData {
+struct ReadState {
     const std::byte *data; // メモリ上のデータの開始位置
     sf_count_t size;       // データの総サイズ
     sf_count_t offset;     // 現在の読み取り位置
@@ -16,54 +16,65 @@ struct MemoryData {
 
 // データのサイズを取得
 static sf_count_t get_filelen(void *user_data) {
-    auto *mem = static_cast<MemoryData *>(user_data);
-    return mem->size;
+    auto l = static_cast<ReadState *>(user_data)->size;
+    std::cout << "Length: " << l << "\n";
+    return l;
 }
 
 // データの読み取り
 static sf_count_t read_file(void *ptr, sf_count_t count, void *user_data) {
-    auto *mem = static_cast<MemoryData *>(user_data);
-    sf_count_t remaining = mem->size - mem->offset;
+    auto *state = static_cast<ReadState *>(user_data);
+    sf_count_t remaining = state->size - state->offset;
     sf_count_t to_read = std::min(count, remaining);
+    std::cout << count << "Read File: " << to_read << "\n";
 
-    std::memcpy(ptr, mem->data + mem->offset, to_read);
-    mem->offset += to_read;
+    std::memcpy(ptr, state->data + state->offset, to_read);
+    state->offset += to_read;
 
+    std::cout << "end\n";
     return to_read;
 }
 
 // データのシーク
 static sf_count_t seek_file(sf_count_t offset, int whence, void *user_data) {
-    auto *mem = static_cast<MemoryData *>(user_data);
+    std::cout << "Seek: " << offset << "\n";
+    auto *state = static_cast<ReadState *>(user_data);
 
-    sf_count_t new_offset = mem->offset;
+    sf_count_t new_offset = state->offset;
     if (whence == SEEK_SET) {
         new_offset = offset;
     } else if (whence == SEEK_CUR) {
         new_offset += offset;
     } else if (whence == SEEK_END) {
-        new_offset = mem->size + offset;
+        new_offset = state->size + offset;
     } else {
         return -1; // 無効なシーク
     }
 
-    if (new_offset < 0 || new_offset > mem->size) {
+    if (new_offset < 0 || new_offset > state->size) {
+        std::cout << "🥺\n";
         return -1; // 範囲外
     }
 
-    mem->offset = new_offset;
-    return mem->offset;
+    std::cout << "ok\n";
+    return state->offset = new_offset;
 }
 
-SF_VIRTUAL_IO virtual_io = {
+static sf_count_t tell(void *user_data) {
+    auto offset = static_cast<ReadState *>(user_data)->offset;
+    std::cout << "Tell: " << offset << "\n";
+    return offset;
+}
+
+static SF_VIRTUAL_IO virtual_io = {
     get_filelen,
     seek_file,
     read_file,
-    nullptr, nullptr};
+    nullptr, tell};
 
 static void play_sound(std::vector<std::byte> &&wav_data) {
     // MemoryData構造体を初期化
-    MemoryData memory_data = {wav_data.data(), static_cast<sf_count_t>(wav_data.size()), 0};
+    ReadState memory_data = {wav_data.data(), static_cast<sf_count_t>(wav_data.size()), 0};
 
     // SF_INFO構造体をゼロ初期化
     SF_INFO sfinfo = {0};
