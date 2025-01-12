eb5bf8ed6c12cac386430fbe897503b49e628ab6
Haruo <haruo-mtok@outlook.com>
2025-01-13 03:26:08 +0900

    [catastrophic] luatest消しちゃう

--- assets/scripts/luatest.lua
+++
@@ -1,7 +0,0 @@
--- プロパティのテスト
----@diagnostic disable: undefined-global
-
--- Usage example
-local hoge = new_hoge()
-hoge.position = { 3, 7 }
-print(hoge.position.x)
--- src/bin/luatest2.cpp
+++
@@ -1,8 +0,0 @@
-#include <luagui.hpp>
-
-int main() {
-    b2SetLengthUnitsPerMeter(0.02f);
-
-    LuaGUI luagui;
-    luagui.run("assets/scripts/luatest2.lua");
-}
