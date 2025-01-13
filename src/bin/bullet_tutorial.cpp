c02732d6c2671dad7a1648c32c00b44faa06254c
Haruo <haruo-mtok@outlook.com>
2025-01-13 19:58:44 +0900

    スマートポインタ使用

--- src/bin/bullet_tutorial.cpp
+++ src/bin/bullet_tutorial.cpp
@@ -1,34 +1,25 @@
 #include <btBulletDynamicsCommon.h>
-#include <iostream>
+#include <memory>
 
 int main() {
     // Bulletの初期化
-    btDefaultCollisionConfiguration *collisionConfiguration = new btDefaultCollisionConfiguration();
-    btCollisionDispatcher *dispatcher = new btCollisionDispatcher(collisionConfiguration);
-    btBroadphaseInterface *overlappingPairCache = new btDbvtBroadphase();
-    btSequentialImpulseConstraintSolver *solver = new btSequentialImpulseConstraintSolver;
-    btDiscreteDynamicsWorld *dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+    auto collision_configuration = std::make_unique<btDefaultCollisionConfiguration>();
+    auto dispatcher = std::make_unique<btCollisionDispatcher>(collision_configuration.get());
+    auto overlapping_pair_cache = std::make_unique<btDbvtBroadphase>();
+    auto solver = std::make_unique<btSequentialImpulseConstraintSolver>();
+    auto dynamics_world = std::make_unique<btDiscreteDynamicsWorld>(dispatcher.get(), overlapping_pair_cache.get(), solver.get(), collision_configuration.get());
 
-    dynamicsWorld->setGravity(btVector3(0, -10, 0));
+    dynamics_world->setGravity(btVector3(0, -10, 0));
 
     // 地面の作成
-    btCollisionShape *groundShape = new btStaticPlaneShape(btVector3(0, 1, 0), 1);
-    btDefaultMotionState *groundMotionState = new btDefaultMotionState(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
-    btRigidBody::btRigidBodyConstructionInfo groundRigidBodyCI(0, groundMotionState, groundShape, btVector3(0, 0, 0));
-    btRigidBody *groundRigidBody = new btRigidBody(groundRigidBodyCI);
-    dynamicsWorld->addRigidBody(groundRigidBody);
+    auto ground_shape = std::make_unique<btStaticPlaneShape>(btVector3(0, 1, 0), 1);
+    auto ground_motion_state = std::make_unique<btDefaultMotionState>(btTransform(btQuaternion(0, 0, 0, 1), btVector3(0, -1, 0)));
+    btRigidBody::btRigidBodyConstructionInfo ground_rigid_body_ci(0, ground_motion_state.get(), ground_shape.get(), btVector3(0, 0, 0));
+    auto ground_rigid_body = std::make_unique<btRigidBody>(ground_rigid_body_ci);
+    dynamics_world->addRigidBody(ground_rigid_body.get());
 
     // シミュレーションのステップ
     for (int i = 0; i < 150; i++) {
-        dynamicsWorld->stepSimulation(1 / 60.f, 10);
+        dynamics_world->stepSimulation(1 / 60.f, 10);
     }
-
-    // クリーンアップ
-    delete dynamicsWorld;
-    delete solver;
-    delete overlappingPairCache;
-    delete dispatcher;
-    delete collisionConfiguration;
-
-    return 0;
 }
