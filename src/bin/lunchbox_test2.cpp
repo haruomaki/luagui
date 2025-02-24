749841479eca00d6466e494ccae62c48b350d84e
Haruo <haruo-mtok@outlook.com>
2025-02-24 21:16:29 +0900

    glTF読み込みはじめ

---
+++ src/bin/lunchbox_test2.cpp
@@ -0,0 +1,25 @@
+#include <Lunchbox/OpenAL.hpp>
+#include <Lunchbox/Storage.hpp>
+#include <Lunchbox/core/third-party/tiny_gltf.h>
+#include <SumiGL/misc.hpp>
+
+int main() {
+    setUTF8();
+
+    OpenAL::Context openal_ctx;
+    lunchbox::Storage storage;
+
+    const auto sound = storage.get_sound("audio/テスト音声.wav");
+
+    // OpenALバッファとソース作成
+    OpenAL::Buffer al_buffer(sound.wave, sound.channels == 1 ? AL_FORMAT_MONO16 : AL_FORMAT_STEREO16, sound.samplerate);
+    OpenAL::Source source(al_buffer);
+
+    // 再生
+    source.play();
+    source.set_gain(1); // 音量調節も可能
+    std::cout << "再生中...\n";
+
+    // 再生が終了するまで待機
+    while (source.is_playing());
+}
