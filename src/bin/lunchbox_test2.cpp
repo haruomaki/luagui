96adb592f65d07cbc2f39899b621834fd235cf90
Haruo <haruo-mtok@outlook.com>
2025-02-25 22:06:50 +0900

    Ninjaでビルド

--- src/bin/lunchbox_test2.cpp
+++ src/bin/lunchbox_test2.cpp
@@ -5,21 +5,23 @@
 
 int main() {
     setUTF8();
+    std::cout << "こんにちは\n";
 
     OpenAL::Context openal_ctx;
     lunchbox::Storage storage;
+    std::cout << storage.get_text("shaders/hello.vsh");
 
     const auto sound = storage.get_sound("audio/テスト音声.wav");
 
-    // OpenALバッファとソース作成
-    OpenAL::Buffer al_buffer(sound.wave, sound.channels == 1 ? AL_FORMAT_MONO16 : AL_FORMAT_STEREO16, sound.samplerate);
-    OpenAL::Source source(al_buffer);
+    // // OpenALバッファとソース作成
+    // OpenAL::Buffer al_buffer(sound.wave, sound.channels == 1 ? AL_FORMAT_MONO16 : AL_FORMAT_STEREO16, sound.samplerate);
+    // OpenAL::Source source(al_buffer);
 
-    // 再生
-    source.play();
-    source.set_gain(1); // 音量調節も可能
-    std::cout << "再生中...\n";
+    // // 再生
+    // source.play();
+    // source.set_gain(1); // 音量調節も可能
+    // std::cout << "再生中...\n";
 
-    // 再生が終了するまで待機
-    while (source.is_playing());
+    // // 再生が終了するまで待機
+    // while (source.is_playing());
 }
