71fd036be658c14d62c8feb9ed23f32308fad945
Haruo <haruo-mtok@outlook.com>
2025-02-26 16:27:49 +0900

    print廃止

--- src/bin/lunchbox_test2.cpp
+++ src/bin/lunchbox_test2.cpp
@@ -1,14 +1,19 @@
 #include <Lunchbox/core/glTF.hpp>
+#include <iostream>
 
 int main() {
     tinygltf::Model model;
-    if (lunchbox::core::load_model(model, "assets/models/cube.glb")) {
+    if (gltf::load_model(model, "assets/models/cube.glb")) {
         std::cout << "Model loaded successfully!" << std::endl;
     } else {
         std::cout << "Failed to load model." << std::endl;
     }
 
-    lunchbox::core::print_vertex_data(model);
-    lunchbox::core::print_uv_data(model);
-    lunchbox::core::print_index_data(model);
+    for (size_t i = 0; auto v : gltf::vertex_data(model)) {
+        std::cout << "Vertexv " << i++ << ": " << v.x << " "
+                  << v.y << " "
+                  << v.z << std::endl;
+    }
+    gltf::uv_data(model);
+    gltf::index_data(model);
 }
